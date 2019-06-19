5bb3b8889b7d425fbfcf8129d7be4f8f235d1c70
allyson <vhassan@outlook.com>
2019-06-18 22:08:55 -0300

    versão estável
    sem bugs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://semana:semana@cluster0-ekbux.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://semana:semana@cluster0-3d4nh.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
 });
 
-app.use(require('./routes'));
 
-app.listen(3335);
+app.get('/', (req, res) => {
+    return res.send(`ola ${req.query.name}`);
+});
+//app.use(require('./routes'));
+
+app.listen(20000);
